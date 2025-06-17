eb6b54b092af0215d4886511bfef19b73a811e8c
pranavmuttevi <niblet220809@gmail.com>
2025-06-17 18:21:15 +0530

    operations

---
+++ operations.py
@@ -0,0 +1,13 @@
+
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+
+sum_of_trees = tree1 + tree2 + tree3 + tree4 + tree5
+print("The sum of all the 5 trees is:", sum_of_trees)
+
+average = sum_of_trees / 5
+print("The average of all the trees is:", average)
